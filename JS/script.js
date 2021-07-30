b55eb2b26c2050e4f81689d4750c6adbe098b80f
Akshay Satpute <87091884+akshaybsatpute@users.noreply.github.com>
2021-07-30 11:43:57 +0530

    JS UPDATED

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,6 @@ let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
-
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 
